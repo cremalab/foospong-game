4bfea4ff382fc2e28bd09efe7f43a52cfdfdc4e5
Deric Mendez <deric@cremalab.com>
2018-07-13 16:36:49 -0500

    add websockets to game

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,73 @@
-import './global.css';
+import "./global.css";
 // import Nes from 'nes';
-import Pong from './pong/Pong.js';
+import Pong from "./pong/Pong.js";
+import Nes from "nes";
 
 console.dir(Pong);
 
-var pong = new Pong(document.getElementById('root'));
+var pong = new Pong(document.getElementById("root"));
+const client = new Nes.Client("ws://10.100.52.166:8000");
+
+const start = async () => {
+  await client.connect();
+  const handler = playerNumber => (update, flags) => {
+    // update -> { id: 5, status: 'complete' }
+    // Second publish is not received (doesn't match)
+    pong.players[playerNumber].keyboard.setKeyState(
+      "up",
+      update.event === "up_press"
+    );
+    pong.players[playerNumber].keyboard.setKeyState(
+      "down",
+      update.event === "down_press"
+    );
+  };
+
+  client.subscribe("/player/1", handler("a"));
+  client.subscribe("/player/2", handler("b"));
+  client.subscribe("/player/3", handler("c"));
+  client.subscribe("/player/4", handler("d"));
+};
+
+start();
+
+console.log(pong.players);
 
 // Add keyboard controls
-// pong.players.a.addControls({
-//   'up': 'q',
-//   'down': 'a',
-// });
-// pong.players.b.addControls({
-//   'up': 'w',
-//   'down': 's',
-// });
-// pong.players.c.addControls({
-//   'up': 'e',
-//   'down': 'd',
-// });
-// pong.players.d.addControls({
-//   'up': 'up',
-//   'down': 'down',
-// });
+pong.players.a.addControls({
+  up: "up",
+  down: "down"
+});
+pong.players.b.addControls({
+  up: "up",
+  down: "down"
+});
+pong.players.c.addControls({
+  up: "up",
+  down: "down"
+});
+pong.players.d.addControls({
+  up: "up",
+  down: "down"
+});
 
-function setAIBehaviorForPlayer(playerName: string) {
-  // Add behaviour for player B
-  pong.on('update', function () {
-    if (Math.random() > 0.8) return;
-    if (pong.players[playerName].y < pong.balls[0].y) {
-      pong.players[playerName].move(1);
-    } else if (pong.players[playerName].y > pong.balls[0].y) {
-      pong.players[playerName].move(-1);
-    }
-  });
-}
+// function setAIBehaviorForPlayer(playerName: string) {
+//   // Add behaviour for player B
+//   pong.on("update", function() {
+//     if (Math.random() > 0.8) return;
+//     if (pong.players[playerName].y < pong.balls[0].y) {
+//       pong.players[playerName].move(1);
+//     } else if (pong.players[playerName].y > pong.balls[0].y) {
+//       pong.players[playerName].move(-1);
+//     }
+//   });
+// }
 
-setAIBehaviorForPlayer('a');
-setAIBehaviorForPlayer('b');
-setAIBehaviorForPlayer('c');
-setAIBehaviorForPlayer('d');
+// setAIBehaviorForPlayer("a");
+// setAIBehaviorForPlayer("b");
+// setAIBehaviorForPlayer("c");
+// setAIBehaviorForPlayer("d");
+
 // async function doSocket() {
 // const client = new Nes.Client('ws://df8e47a1.ngrok.io');
 
